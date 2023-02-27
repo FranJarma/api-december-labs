123dcf0cf3755f126f023f3d593cf7c57e9849bc
Francisco Jarma <francisco.jarma@onetree.com>
2023-02-27 15:13:30 -0300

    Erased unecesary log

--- controllers/ReportController.ts
+++ controllers/ReportController.ts
@@ -7,7 +7,6 @@ export const GetReportsByUser = async (req: Request, res: Response) => {
         const from: Date = req.body.from;
         const to: Date = req.body.to || null;
         const report = await getReportsByUser(from, to);
-        console.log(report);
         sendHttpStatus(res, 200, report);
     } catch (error) {
         sendHttpStatus(res, 500);
